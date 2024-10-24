7d43d0428a89b54df87c52f369ac8c41416f356c
Theodorbjorkner <theodor.bjorkner@gmail.com>
2024-10-24 15:20:05 +0200

    created get / path  created Node.js server

--- src/server.js
+++ src/server.js
@@ -10,6 +10,21 @@ function startServer(port){
     });
 }
 
+app.get("/", (req, res) => {
+    // calculate running time
+    const timeStamp = getTimestamp();
+    const runningTime= timeStamp - serverStartTimestamp;
+    const runningTimeSeconds = Math.floor(runningTime / 1000);
+
+    // generate response message
+    const responseMessage = 'The time is now ${timeStamp}<br />' + 
+    'the server is running for ${runningTimeSeconds} seconds <br />' + 
+    'App version ${process.env.npm_package_version}';
+  // sending the response
+  res.statusCode = 200;
+  res.send(responseMessage); 
+});
+
 function getTimestamp(){
     const dateNow = new Date();
     const timeStamp = dateNow.getTime();
